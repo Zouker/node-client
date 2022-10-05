539edfdeba57a1c71577ed6fb09da3eac6968381
Denis Shubin <denis.shubin@gmail.com>
2022-10-06 00:35:47 +0300

    feat: add CRUD operations

--- src/App.js
+++ src/App.js
@@ -6,9 +6,10 @@ function App() {
 
     const [users, setUsers] = useState([]);
     const [value, setValue] = useState([]);
+    const [editMode, setEditMode] = useState(false);
     const inputRef = useRef(null);
     const getUsers = () => {
-        axios.get('http://localhost:7542/users').then(res => {
+        axios.get('http://localhost:7542/users' + window.location.search).then(res => {
             setUsers(res.data);
         })
     };
@@ -17,21 +18,42 @@ function App() {
         getUsers();
     }, []);
 
-    let createUser = (event) => {
+    const createUser = () => {
         axios.post('http://localhost:7542/users', {name: inputRef.current.value}).then(res => {
             getUsers();
         })
-        setValue('')
+        setValue([])
     };
 
-    let onChange = (e) => {
+    const deleteUser = (id) => {
+        axios.delete(`http://localhost:7542/users/${id}`).then(res => {
+            getUsers();
+        })
+        setValue([])
+    }
+
+    const updateUser = (id, name) => {
+        axios.put('http://localhost:7542/users/', {id, name}).then(res => {
+            getUsers();
+        })
+    };
+
+    const onChange = (e) => {
         setValue(e.currentTarget.value)
     };
 
     return <div>
         <input onChange={onChange} value={value} ref={inputRef}/>
         <button onClick={createUser}>Create User</button>
-        {users.map(u => <div>{u.name}</div>)}
+        {users.map(u => <div>
+            {editMode
+                ? <input defaultValue={u.name} onBlur={(e) => {
+                    updateUser(u._id, e.currentTarget.value)
+                    setEditMode(false)
+                }}/>
+                : <span onDoubleClick={()=>setEditMode(true)}>{u.name}</span>}
+            <button onClick={() => deleteUser(u._id)}>x</button>
+        </div>)}
     </div>
 }
 
